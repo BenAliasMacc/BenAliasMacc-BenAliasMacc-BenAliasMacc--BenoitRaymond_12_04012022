887a8918c5de83c78411e80304832da9eb5e7f1d
BenAliasMacc <80431649+BenAliasMacc@users.noreply.github.com>
2022-01-12 10:52:29 +0100

    chart activity

--- sportsee/src/component/Activity/index.jsx
+++ sportsee/src/component/Activity/index.jsx
@@ -3,21 +3,27 @@ import React from 'react'
 import './index.css'
 
 function Activity({ userActivity }) {
-    console.log(userActivity);
+    const formatDay = (item) => (new Date(item)).getDate()
 
-    
+    const CustomTooltip = ({ active, payload }) => active ? (
+        <div className="chart-tooltip">
+            <div>{payload[0].value}kg</div>
+            <div>{payload[1].value}kCal</div>
+        </div>
+    ) : null
 
     return (
         <div className="activityChart">
             <h2>Activité quotidienne</h2>
             <BarChart width={835} height={320} data={userActivity} margin={{left: 32}}>
                 <CartesianGrid vertical={false} strokeDasharray="3 3" />
-                <XAxis dataKey="day" />
-                <YAxis orientation="right" tickCount={3} />
-                <Tooltip contentStyle={{backgroundColor: 'red', color: 'white'}}/>
-                <Legend verticalAlign="top" align="right" height={96} margin={{top: 100, right: 5, bottom: 5, left: 5 }}/>
-                <Bar legendType="circle" dataKey="kilogram" fill="#282D30" name='Poids (kg)' barSize={7} type="monotone"/>
-                <Bar legendType="circle" dataKey="calories" fill="#E60000" name='Calories brûlées (kCal)' barSize={7} type="monotone"/>
+                <XAxis dataKey="day" tickFormatter={formatDay}/>
+                <YAxis yAxisId="kilogram" orientation="right" domain={['dataMin-5', 'dataMax+0']} tickCount={3} fill='#9B9EAC'/>
+                <YAxis yAxisId="calories" hide domain={['dataMin-100', 'dataMax+0']} />
+                <Tooltip content={<CustomTooltip />}/>
+                <Legend verticalAlign="top" align="right" height={96}/>
+                <Bar yAxisId="kilogram" legendType="circle" dataKey="kilogram" fill="#282D30" name='Poids (kg)' barSize={7} radius={[5, 5, 0, 0]}/>
+                <Bar yAxisId="calories" legendType="circle" dataKey="calories" fill="#E60000" name='Calories brûlées (kCal)' barSize={7} radius={[5, 5, 0, 0]}/>
             </BarChart>
         </div>
     )
